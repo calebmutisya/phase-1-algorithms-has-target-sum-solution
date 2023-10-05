9f6d4f6aa9cfecb7c6521a311e361459745f32be
calebmutisya <mutisyacalebmusau@gmail.com>
2023-10-05 15:50:45 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // A set to store numbers as we iterate through them
+  const numSet= new Set();
+  for(const num of array){
+    //Calculate the compliment for the current number
+    const compliment= target-num;
+    //If the complement is in the set,we found a pair that adds up to the target
+    if(numSet.has(compliment)){
+      return true;
+    }
+    numSet.add(num);
+  }
+  return false;
 }
 
 /* 
@@ -8,6 +19,8 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  1)The function takes each number in an array and adds them to each other in a pair
+  2) if result is equal to target it returns true else it returns false
 */
 
 /*
